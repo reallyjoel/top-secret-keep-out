9669843d9e655a5de1a4e25a7e681252c32ed12b
Joel Nyström <joel@ludosity.com>
2016-09-18 17:54:34 +0200

    Main menu placeholder trams

---
+++ Top Secret Keep Out/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("test");
+        }
+	}
+}
