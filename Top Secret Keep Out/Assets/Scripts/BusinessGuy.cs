12ceb49d5aec100bec03fc176261af2b083bd4c8
Joel Nyström <joel@ludosity.com>
2016-09-18 18:57:25 +0200

    Basic running and jumping

---
+++ Top Secret Keep Out/Assets/Scripts/BusinessGuy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BusinessGuy : MonoBehaviour
+{
+    public float RunSpeed = 0f;
+    public float JumpForce = 0f;
+
+    Rigidbody2D body;
+
+	void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector2.left * RunSpeed);
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            body.AddForce(Vector2.up * JumpForce);
+        }
+	}
+}
